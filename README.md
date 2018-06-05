c9baff6d043f910879a1b89d7bd188761a2175bf
katapad <katapad@gmail.com>
2018-06-05 19:16:03 +0900

    query

--- README.md
+++ README.md
@@ -11,6 +11,9 @@ npm install
 # serve with hot reload at localhost:8080
 npm run dev
 
+# serve with hot reload at 192.168.11.91:8080
+npm run dev -- --host 192.168.11.91
+
 # build for production with minification
 npm run build
 ```
--- src/Util.js
+++ src/Util.js
@@ -1,5 +1,7 @@
+const queryString = require('query-string');
+const query = queryString.parse(location.search);
 export default class Util {
   static init() {
-    Util.socket = "192.168.11.91";
+    Util.socket = query.ip || "192.168.11.91";
   }
 }
